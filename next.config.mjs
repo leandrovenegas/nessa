2e109ce2bddfd376ca2d63ab6bbc242aeaf697c1
leandrovenegas <leandrovenegasoficila@gmail.com>
2026-07-07 01:11:29 -0400

    segundo commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  allowedDevOrigins: ['192.168.1.88'],
 };
 
 export default nextConfig;
